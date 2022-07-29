4e61a68a5a419f8ac5912c76ee9ebef913ce66ec
zonde306 <13820323+zonde306@users.noreply.github.com>
2022-07-29 20:38:23 +0800

    init

---
+++ CellControllerPatch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace RF5_AxeHarvest
+{
+	[HarmonyPatch(typeof(Farm.CellController), nameof(Farm.CellController.CanChop))]
+	public class CellControllerCanChop
+	{
+		static bool Prefix(Farm.CellController __instance, ref bool __result)
+		{
+			__result = __instance.plantStatus.PlantStatusLevel > 0 && (__instance.CropData.CropType & (int)CROP_TYPE.RF4_CROP_TYPE_TREE) != 0;
+			__result = __result || __instance.plantStatus.IsStatusMax();
+			return false;
+		}
+	}
+
+	[HarmonyPatch(typeof(Farm.CellController), nameof(Farm.CellController.DoChop))]
+	public class CellControllerDoChop
+	{
+		static bool Prefix(HumanController humanController, Farm.CellController __instance)
+		{
+			// 如果是树且没有果实
+			if ((__instance.CropData.CropType & (int)CROP_TYPE.RF4_CROP_TYPE_TREE) != 0 &&
+				!Farm.FarmManager.Instance.rf4FreeFarmCropPickupOkCheck(__instance.FarmId, __instance.CellSetId, __instance.CellId, true))
+				return true;
+
+			__instance.Harvest(humanController, true);
+			return false;
+		}
+	}
+}
---
+++ Main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+using BepInEx.IL2CPP;
+using BepInEx.Logging;
+
+namespace RF5_AxeHarvest
+{
+	[BepInPlugin(GUID, NAME, VERSION)]
+	[BepInProcess(GAME_PROCESS)]
+	public class Main : BasePlugin
+	{
+		#region PluginInfo
+		private const string GUID = "34A85B08-B01E-B507-8262-32619D84AA37";
+		private const string NAME = "RF5_AxeHarvest";
+		private const string VERSION = "1.0";
+		private const string GAME_PROCESS = "Rune Factory 5.exe";
+		#endregion
+
+		public static new ManualLogSource Log;
+
+		public override void Load()
+		{
+			Log = base.Log;
+			new Harmony(GUID).PatchAll();
+		}
+	}
+}
